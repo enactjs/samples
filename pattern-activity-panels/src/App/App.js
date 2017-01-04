2cfc765fad6db05b629e03cfdd056e5ba4b2df4d
Stephen Choi <stephen.choi@lge.com>
2017-01-03 16:46:52 -0800

    PLAT-32048: Add ActivityPanels sample
    Enyo-DCO-1.1-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

---
+++ pattern-activity-panels/src/App/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
+import {ActivityPanels} from '@enact/moonstone/Panels';
+import MainPanel from '../views/MainPanel';
+
+class App extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			index: this.props.index || 0
+		}
+	}
+
+	handleSelectBreadcrumb = ({index}) => this.setState({index})
+
+	handleClick = () => this.setState({index: this.state.index + 1})
+
+	render () {
+		return(
+			<ActivityPanels {...this.props} onSelectBreadcrumb={this.handleSelectBreadcrumb} index={this.state.index}>
+				<MainPanel title="First" onClick={this.handleClick}/>
+				<MainPanel title="Second" onClick={this.handleClick}/>
+				<MainPanel title="Third" onClick={this.handleClick}/>
+				<MainPanel title="Fourth" />
+			</ActivityPanels>
+		)
+	}
+}
+
+export default MoonstoneDecorator(App);
