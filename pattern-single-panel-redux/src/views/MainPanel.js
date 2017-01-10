8c8ac083c114a9a4a0694dd96ed8af20a201396c
Teck <teck.liew@lge.com>
2017-01-09 16:30:27 -0800

    refactor to MainView

--- pattern-single-panel-redux/src/views/MainPanel.js
+++ pattern-single-panel-redux/src/views/MainPanel.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import kind from '@enact/core/kind';
 import {Panel, Header} from '@enact/moonstone/Panels';
-import ProfilePhotoPicker from '../components/ProfilePhotoPicker';
+import css from './MainPanel.less';
+
+import PhotoPickerContainer from '../containers/PhotoPickerContainer';
+import PhotoPreviewContainer from '../containers/PhotoPreviewContainer';
+import PhotoSliderContainer from '../containers/PhotoSliderContainer';
 import FooterContainer from '../containers/FooterContainer';
 
 const MainPanel = kind({
@@ -10,7 +14,11 @@ const MainPanel = kind({
 	render: () => (
 		<Panel>
 			<Header title="Profile Photo" titleBelow="Choose your profile picture" type="compact" preserveCase />
-			<ProfilePhotoPicker />
+			<div className={css.profilePhotoPicker}>
+				<PhotoPreviewContainer />
+				<PhotoSliderContainer />
+				<PhotoPickerContainer />
+			</div>
 			<FooterContainer />
 		</Panel>
 	)
