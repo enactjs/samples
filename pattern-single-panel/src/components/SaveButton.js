39245c8a7716f0fb95cc64a07bdbad3ce2b4d4cd
Ryan Duffy <ryan@tiqtech.com>
2017-01-26 09:02:10 -0600

    add prop spread to SaveButton

--- pattern-single-panel/src/components/SaveButton.js
+++ pattern-single-panel/src/components/SaveButton.js
@@ -24,7 +24,7 @@ class SaveButton extends React.Component {
 	}
 
 	render = () => (
-		<div className={css.saveButton}>
+		<div {...this.props} className={css.saveButton}>
 			<Button onClick={this.handleOnSave}>
 				Save
 			</Button>
