7aa37a60eeee4c9525de6cb65b9c59651f3d9f2a
Dave Freeman <dave.freeman@lge.com>
2017-03-07 11:51:13 -0600

    fixup and munge again
    Ryan's changes + more

--- pattern-dynamic-panel/src/App/App.js
+++ pattern-dynamic-panel/src/App/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import kind from '@enact/core/kind';
+import Changeable from '@enact/ui/Changeable';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 
 import FileBrowser from '../components/FileBrowser';
 import css from './App.less';
+
+// This would be replaced by redux but Changeable is a handy single-value, single-event state HOC
+const Browser = Changeable(
+	{prop: 'path', change: 'onNavigate'},
+	FileBrowser
+);
 
 const App = kind({
 	name: 'App',
@@ -15,7 +22,7 @@ const App = kind({
 
 	render: (props) => (
 		<div {...props}>
-			<FileBrowser path="/a/b/c" />
+			<Browser defaultPath="/a" />
 		</div>
 	)
 });
--- pattern-dynamic-panel/src/components/FileBrowser/FileBrowser.js
+++ pattern-dynamic-panel/src/components/FileBrowser/FileBrowser.js
@@ -2,11 +2,10 @@ import React from 'react';
 import kind from '@enact/core/kind';
 import ri from '@enact/ui/resolution'
 import {Item} from '@enact/moonstone/Item';
-import {Spottable} from '@enact/spotlight';
+import Cancelable from '@enact/ui/Cancelable';
 import {VirtualList} from '@enact/moonstone/VirtualList'
 
 import DynamicPanel from '../DynamicPanel';
-import Navigable from '../Navigable';
 
 const cDir = ['foo3.txt'];
 const bDir = [cDir, 'bar.txt'];
@@ -15,40 +14,49 @@ const aDir = [bDir, 'foo.txt'];
 const a = {
 	files: [
 		{name: 'b', directory: true},
-		{name: 'cat.jpg'},
-		{name: 'dog.jpg'}
+		{name: 'rainbow.jpg'},
+		{name: 'macaw.jpg'}
 	]
 };
 
 const b = {
 	files: [
 		{name: 'c', directory: true},
-		{name: '123.jpg'},
-		{name: 'abc.jpg'}
+		{name: 'jellyfish.jpg'},
+		{name: 'butterfly.jpg'}
 	]
 };
 
 const c = {
 	files: [
-		{name: 'foo.jpg'},
-		{name: 'bar.jpg'}
+		{name: 'ornaments.jpg'},
+		{name: 'frozen-waterfall.jpg'}
 	]
 };
 
 const scale = ri.scale;
 
-const clickTest = ({...rest}) => {
-	console.log('CLICKY CLICKY', rest);
-}
-
 const FileBrowserBase = kind({
 	name: 'FileBrowserBase',
+	handlers: {
+		// create a cached event handler forwarding to onNavigate
+		onNavigate: (ev, props) => {
+			// extract the index provided by VirtualList
+			const index = ev.currentTarget.dataset.index;
+			// map it to the name from the hardcoded list of files
+			const name = a.files[index].name;
+			// make the new path
+			const path = `${props.path}/${name}`;
+			// and notify the handler
+			props.onNavigate({path});
+		}
+	},
 	computed: {
-		listItem: (props) => ({data, index, key, ...inner}) => {
-			console.log('LIST ITEM OUTER PROPS', props);
-			console.log('LIST ITEM INNER PROPS', Object.assign({data, index, key}, inner));
+		// this double function is messy but i'm assuming it's a requirement of VirtualList ... we
+		// should fix that API
+		listItem: (props) => ({data, index, key, ...rest}) => {
 			return (
-				<Item key={key} onClick={props.onNavigate} >
+				<Item key={key} onClick={props.onNavigate} {...rest}>
 					{data[index].name}
 				</Item>
 			);
@@ -56,9 +64,9 @@ const FileBrowserBase = kind({
 	},
 	render: (props) => {
 		const {listItem, ...rest} = props;
-		console.log('FileBrowserBase', props);
+
 		return (
-			<DynamicPanel {...rest} >
+			<DynamicPanel {...rest}>
 				<VirtualList
 					itemSize={scale(72)}
 					component={listItem}
@@ -70,30 +78,51 @@ const FileBrowserBase = kind({
 	}
 });
 
-const FB = class extends React.Component {
-	// constructor (props) {
-	// 	super(props);
-	//
-	// 	this.state = {
-	// 		path: props.path
-	// 	}
-	// }
+const popPath = (path) => {
+	let newPath = '/';
+	let lastPath = path;
 
-	navigate ({path}) {
-		console.log('FileBrowser onNavigate', path);
-		this.setState({path: path});
+	if (path) {
+		const parts = path.split('/').filter((e) => e);
+		lastPath = parts.pop();
+		if (parts.length) {
+			newPath += parts.join('/');
+			if (!newPath) {
+				newPath = '/';
+			}
+		} else {
+			// nowhere to go
+			newPath += lastPath ? lastPath : '';
+		}
 	}
+	return newPath;
+};
 
-	render () {
-		const props = Object.assign({}, this.props);
-		console.log('STATEFUL FileBrowser RENDERING', props);
-		return (
-			<FileBrowserBase {...props} />
-		);
+// the onCancel callback from the Cancelable config receives the Cancelable's props to both
+// determine if it should cancel and how to handle the cancel. here, we're calling the onNavigate
+// event callback.
+const handleCancel = (props) => {
+	const {path, onNavigate} = props;
+	// pop the path
+	const newPath = popPath(path);
+	// and if there's an onNavigate callback
+	if (onNavigate) {
+		// call it with the new path
+		onNavigate({
+			path: newPath
+		});
+
+		// then return true to indicate it was handled
+		return true;
 	}
 };
 
-const FileBrowser = Navigable(Spottable(FB));
+const FileBrowser = Cancelable(
+	// modal lets it handle any cancel event that isn't handled by the spotted component. Useful for
+	// full-screen containers like Panels (or Panel in this case)
+	{modal: true, onCancel: handleCancel},
+	FileBrowserBase
+);
 
 export default FileBrowser;
 export {FileBrowser, FileBrowserBase};
