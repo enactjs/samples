2653708c45835e2ac408c7761508d295a355f6f5
Dave Freeman <dave.freeman@lge.com>
2017-03-07 12:13:02 -0600

    ryan's changeable change

--- pattern-dynamic-panel/src/App/App.js
+++ pattern-dynamic-panel/src/App/App.js
@@ -1,11 +1,18 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import kind from '@enact/core/kind';
+import Changeable from '@enact/ui/Changeable';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 
 import MainView from '../views/MainView';
+import FileBrowser from '../components/FileBrowser';
 import css from './App.less';
 
+// This would be replaced by redux but Changeable is a handy single-value, single-event state HOC
+const Browser = Changeable(
+	{prop: 'path', change: 'onNavigate'},
+	FileBrowser
+);
 import {navigate} from '../actions';
 
 const App = kind({
@@ -24,6 +31,11 @@ const App = kind({
 		className: 'app'
 	},
 
+	// render: (props) => (
+	// 	<div {...props}>
+	// 		<Browser defaultPath="/a" />
+	// 	</div>
+	// ),
 	render: ({path, ...rest}) => (
 		<div {...rest}>
 			<MainView path={path} />
--- pattern-dynamic-panel/src/components/FileBrowser/FileBrowser.js
+++ pattern-dynamic-panel/src/components/FileBrowser/FileBrowser.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import kind from '@enact/core/kind';
 import ri from '@enact/ui/resolution'
-import {Cancelable} from '@enact/ui/Cancelable';
 import {Item} from '@enact/moonstone/Item';
-import {VirtualList} from '@enact/moonstone/VirtualList';
+import Cancelable from '@enact/ui/Cancelable';
+import {VirtualList} from '@enact/moonstone/VirtualList'
 
 const cDir = ['foo3.txt'];
 const bDir = [cDir, 'bar.txt'];
@@ -34,6 +34,60 @@ const c = {
 
 const scale = ri.scale;
 
+const FileBrowserBase = kind({
+	name: 'FileBrowserBase',
+	handlers: {
+		// create a cached event handler forwarding to onNavigate
+		onNavigate: (ev, props) => {
+			// extract the index provided by VirtualList
+			const index = ev.currentTarget.dataset.index;
+			// map it to the name from the hardcoded list of files
+			const name = a.files[index].name;
+			// make the new path
+			const path = `${props.path}/${name}`;
+			// and notify the handler
+			props.onNavigate({path});
+		}
+	},
+	computed: {
+		// this double function is messy but i'm assuming it's a requirement of VirtualList ... we
+		// should fix that API
+		listItem: (props) => ({data, index, key, ...rest}) => {
+			return (
+				<Item key={key} onClick={props.onNavigate} {...rest}>
+					{data[index].name}
+				</Item>
+			);
+		}
+	},
+	// render: (props) => {
+	// 	const {listItem, ...rest} = props;
+	//
+	// 	return (
+	// 		<DynamicPanel {...rest}>
+	// 			<VirtualList
+	// 				itemSize={scale(72)}
+	// 				component={listItem}
+	// 				data={a.files}
+	// 				dataSize={a.files.length}
+	// 			/>
+	// 		</DynamicPanel>
+	// 	);
+	// },
+	render: ({listItem, onKeyUp, ...rest}) => {
+		console.log('FILEBROWSER BASE RENDER', rest);
+		console.log('HAVE ONKEYUP', onKeyUp);
+		return (
+			<VirtualList
+				itemSize={scale(72)}
+				component={listItem}
+				data={a.files}
+				dataSize={a.files.length}
+			/>
+		);
+	}
+});
+
 const popPath = (path) => {
 	let newPath = '/';
 	let lastPath = path;
@@ -54,56 +108,31 @@ const popPath = (path) => {
 	return newPath;
 };
 
-const navOrView = ({...rest}) => {
-	console.log('CHOOSE TO NAV TO NEW DIR OR VIEW FILE', rest);
+// the onCancel callback from the Cancelable config receives the Cancelable's props to both
+// determine if it should cancel and how to handle the cancel. here, we're calling the onNavigate
+// event callback.
+const handleCancel = (props) => {
+	const {path, onNavigate} = props;
+	// pop the path
+	const newPath = popPath(path);
+	// and if there's an onNavigate callback
+	if (onNavigate) {
+		// call it with the new path
+		onNavigate({
+			path: newPath
+		});
+
+		// then return true to indicate it was handled
+		return true;
+	}
 };
 
-const cancel = (args) => {
-	console.log('GOOD LUCK', args);
-}
-
-const FileBrowserBase = kind({
-	name: 'FileBrowserBase',
-
-	handlers: {
-		onKeyUp: (ev, {onCancel, onNavigate, path}) => {
-			console.log('HMM');
-		},
-		onNavigate: (ev, {onCancel, onNavigate, path}) => {
-			console.log('HANDLERS HUH', path);
-			console.log('NEED TO DECIDE IF VIEW OR NAV, IF NAV, BACK OR FORWARD');
-			onNavigate({path});
-			console.log('CAN CANCEL', onCancel);
-		}
-	},
-
-	computed: {
-		listItem: ({onNavigate, ...rest}) => ({data, index, key, ...inner}) => {
-			console.log('LIST ITEM OUTER PROPS', Object.assign({onNavigate}, rest));
-			console.log('LIST ITEM INNER PROPS', Object.assign({data, index, key}, inner));
-			return (
-				<Item key={key} onClick={onNavigate} >
-					{data[index].name}
-				</Item>
-			);
-		}
-	},
-
-	render: ({listItem, onKeyUp, ...rest}) => {
-		console.log('FILEBROWSER BASE RENDER', rest);
-		console.log('HAVE ONKEYUP', onKeyUp);
-		return (
-			<VirtualList
-				itemSize={scale(72)}
-				component={listItem}
-				data={a.files}
-				dataSize={a.files.length}
-			/>
-		);
-	}
-});
-
-const FileBrowser = Cancelable({onCancel: 'onNavigate'}, FileBrowserBase);
+const FileBrowser = Cancelable(
+	// modal lets it handle any cancel event that isn't handled by the spotted component. Useful for
+	// full-screen containers like Panels (or Panel in this case)
+	{modal: true, onCancel: handleCancel},
+	FileBrowserBase
+);
 
 export default FileBrowser;
 export {FileBrowser, FileBrowserBase};
