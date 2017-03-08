f6e3a1f807e5a58506c00f71b29caecf7354a101
YunBum SUNG <yb.sung@lge.com>
2017-03-09 07:50:04 +0900

    PLAT-37312: Make a sample for Accessibility feature test app (#19)
    * Add Accessibility Test App
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)
    
    * Clean up
    
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)
    
    * Clean up
    
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)
    
    * Add ramda in package.json
    
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)
    
    * Clean up
    
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)
    
    * Add Picker samples in pattern-a11y
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Add DayPicker,ExpandableItem,ExpandableList,ProgressBar and Slider sample
    
    * Picker clean up
    
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)
    
    * Picker clean up
    
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)
    
    * Clean up progressBar
    
    * Clean up A11yDecorator sample
    
    * add Button sample
    
    * Clean up
    
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)
    
    * Cleanup
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Cleanup dependencies
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * rename pattern-a11y to qa-a11y
    
    * add input sample
    
    * add button aria-labels
    
    * add Item sample
    
    * add item sample 2
    
    * add item sample 3
    
    * add switch sample
    
    * add missing aria-disabled
    
    * update aria's
    
    * columnize InputView
    
    * add empty button and empty item
    
    * update ToggleButton
    
    * fix qa-ally Input sample
    
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)
    
    * Fixes prop bleed
    
    * General cleanup
    
    
    QA-Reviewed-By: Jeff Lam (jeff.lam@lge.com)
    Integrated-By: Aaron Tam (aaron.tam@lge.com)

---
+++ qa-a11y/src/App/App.js
@@ -0,0 +1,70 @@
+import Changeable from '@enact/ui/Changeable';
+import Group from '@enact/ui/Group';
+import Item from '@enact/moonstone/Item';
+import kind from '@enact/core/kind';
+import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
+import React from 'react';
+import Scroller from '@enact/moonstone/Scroller';
+import ViewManager from '@enact/ui/ViewManager';
+
+import css from './App.less';
+import Home from './Home';
+import View from './View';
+
+import A11yDecorator from '../views/A11yDecorator';
+import Button from '../views/Button';
+import DayPicker from '../views/DayPicker';
+import ExpandableItem from '../views/ExpandableItem';
+import ExpandableList from '../views/ExpandableList';
+import Input from '../views/Input';
+import ItemView from '../views/Item';
+import Picker from '../views/Picker';
+import ProgressBar from '../views/ProgressBar';
+import Slider from '../views/Slider';
+
+const views = [
+	{title: 'About A11y', view: Home},
+	{title: 'A11yDecorator', view: A11yDecorator},
+	{title: 'Button', view: Button},
+	{title: 'DayPicker', view: DayPicker},
+	{title: 'ExpandableItem', view: ExpandableItem},
+	{title: 'ExpandableList', view: ExpandableList},
+	{title: 'Input', view: Input},
+	{title: 'Item', view: ItemView},
+	{title: 'Picker', view: Picker},
+	{title: 'ProgressBar', view: ProgressBar},
+	{title: 'Slider', view: Slider}
+];
+
+const AppBase = kind({
+	computed: {
+		handleChange: ({onIndex}) => ({selected}) => {
+			if (selected !== null) {
+				onIndex({index: selected});
+			}
+		}
+	},
+
+	render: ({handleChange, index, ...rest}) => {
+		delete rest.onIndex;
+
+		return (
+			<div {...rest}>
+				<Scroller className={css.nav}>
+					<Group childComponent={Item} itemProps={{className: css.navItem}} onSelect={handleChange} selected={index}>
+						{views.map((view) => view.title)}
+					</Group>
+				</Scroller>
+				<ViewManager className={css.content} index={index}>
+					{views.map((view, i) => (
+						<View {...view} key={i} />
+					))}
+				</ViewManager>
+			</div>
+		);
+	}
+});
+
+const App = MoonstoneDecorator(Changeable({prop: 'index', change: 'onIndex'}, AppBase));
+
+export default App;
