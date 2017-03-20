cc501d7b71e875b9c1f6bd8a07496e0a48de15e1
YunBum SUNG <yb.sung@lge.com>
2017-03-20 21:43:23 +0900

    Add VisibilityObserver
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- pattern-lazy-decorator/src/App.js
+++ pattern-lazy-decorator/src/App.js
@@ -11,6 +11,7 @@ import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 import React, {Component, PropTypes} from 'react';
 import Repeater from '@enact/ui/Repeater';
 import LazyChildDecorator from '@enact/moonstone/LazyChildDecorator';
+import Resizable from '@enact/ui/Resizable';
 import ri from '@enact/ui/resolution';
 import Scroller from '@enact/moonstone/Scroller';
 import {Spottable} from '@enact/spotlight';
@@ -135,10 +136,12 @@ const Message = kind({
 	}
 });
 const ExpandableMessage = LazyChildDecorator(
-	{resize: 'onOpen'},
+	{
+		resize: 'onOpen',
+		initialHeight: 142
+	},
 	Changeable({prop: 'open', change: 'onOpen'}, Message)
 );
-// const LazyRepeater = LazyChildrenDecorator(Repeater);
 
 const longText = $L('Welcome to your new LG webOS UHD Broadcast Box! We appreciate you choosing LG and have designed your new UHD Broadcast Box to make enjoying an unparalleled entertainment experience as simple as possible.<br><br>- Simple Switching: Easily switch between channels, input sources and apps all from your customizable Home screen.<br>- Simple Connections: It’s never been easier to set up your UHD Broadcast Box and connect all the devices you use to watch your favorite shows and movies, play games, listen to music and more.<br>- Simple Discovery: Find programming from all your live and streaming sources that match your taste and interests.<br><br>Visit the LG store to find a wide selection of apps, games, VODs and more. You can also find content by using your UHD Broadcast Box’s built-in search and recommendation functions.');
 const items = [];
