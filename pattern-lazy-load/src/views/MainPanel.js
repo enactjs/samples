96bf13cad04c25d0b35a4aa0ab466e9f90201cdc
Derek Tor <derek.tor@lge.com>
2017-04-28 14:59:26 -0700

    as

--- pattern-lazy-load/src/views/MainPanel.js
+++ pattern-lazy-load/src/views/MainPanel.js
@@ -6,49 +6,13 @@ import Popup from '@enact/moonstone/Popup';
 import Button from '@enact/moonstone/Button';
 
 class ClosePopup extends React.Component {
-	constructor(props){
-		super(props);
-
-		this.state = {
-			isPopupShow: false
-		};
-	}
-
-	componentWillMount () {
-		Spotlight.setPointerMode(false);
-	}
-
-	openPopup = () => {
-		this.setState({
-			isPopupShow: true
-		})
-	}
-
-	closePopup = () => {
-		this.setState({
-			isPopupShow: false
-		})
-	}
-
 	render () {
 		const {onClick, ...rest} = this.props;
 
 		return(
 				<Panel {...rest}>
 					<Header title='Close Popup' />
-						<Button onClick={this.openPopup}>Open Popup</Button>
-						<Button onClick={onClick}>A</Button>
-						<Button>B</Button>
-						<Popup
-							open={this.state.isPopupShow}
-							onClose={this.closePopup}
-							showCloseButton
-						>
-							<div>This is Popup</div>
-							<br />
-							<Button onClick={this.closePopup}>ok</Button>
-							<Button onClick={this.closePopup}>cancel</Button>
-						</Popup>
+					<Button onClick={onClick}>A</Button>
 				</Panel>
 		);
 	}
