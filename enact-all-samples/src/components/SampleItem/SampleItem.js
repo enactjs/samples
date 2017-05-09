7c04c191be1ed99a16fb9fa3b8865bd764b6c925
Derek Tor <derek.tor@lge.com>
2017-05-08 18:07:58 -0700

    fixed up SampleItem component

--- enact-all-samples/src/components/SampleItem/SampleItem.js
+++ enact-all-samples/src/components/SampleItem/SampleItem.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Item from '@enact/moonstone/Item';
 
-const itemSelect = (history, path) => history.push({pathname: path});
+class SampleItem extends React.Component {
+	constructor(props) {
+		super(props)
+	}
 
-const SampleItem = ({children, path, history, rest}) => {
-	return (
-		<Item {...rest} onClick={() => itemSelect(history, path)}>
-			{children}
-		</Item>
-	);
-};
+	itemSelect = () => {
+		this.props.history.push({pathname: this.props.path});
+	}
+
+	render () {
+		const {children, rest} = this.props;
+
+		return (
+			<Item {...rest} onClick={this.itemSelect}>
+				{children}
+			</Item>
+		)
+	}
+}
 
 export default SampleItem;
