1f49e6360c43b82c32d4abe39f12fa0ab515358d
cholanmadala <cholanmadala1510@gmail.com>
2017-05-10 16:21:47 +0530

    PLAT-39880-Created a Pattern sample for ExpandableList with Object support

---
+++ pattern-expandablelist-object/src/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import kind from '@enact/core/kind';
+import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
+import {Panels} from '@enact/moonstone/Panels';
+import MainPanel from '../views/MainPanel';
+import css from './App.less';
+
+const App = kind({
+	name: 'App',
+
+	styles: {
+		css,
+		className: 'app'
+	},
+
+	render: (props) => (
+		<div {...props}>
+			<Panels>
+				<MainPanel />
+			</Panels>
+		</div>
+	)
+});
+
+export default MoonstoneDecorator(App);
