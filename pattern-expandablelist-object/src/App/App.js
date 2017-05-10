3879f8b954cf867c51dcdcb35afecaad9a03f05b
cholanmadala <cholanmadala1510@gmail.com>
2017-05-10 16:52:28 +0530

    PLAT-39880-Clean up

--- pattern-expandablelist-object/src/App/App.js
+++ pattern-expandablelist-object/src/App/App.js
@@ -3,15 +3,9 @@ import kind from '@enact/core/kind';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 import {Panels} from '@enact/moonstone/Panels';
 import MainPanel from '../views/MainPanel';
-import css from './App.less';
 
 const App = kind({
 	name: 'App',
-
-	styles: {
-		css,
-		className: 'app'
-	},
 
 	render: (props) => (
 		<div {...props}>
