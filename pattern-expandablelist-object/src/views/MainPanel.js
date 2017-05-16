143a21d2e324264670388f34038eeb6adc39ef1b
cholanmadala <cholanmadala1510@gmail.com>
2017-05-16 15:07:23 +0530

    Pattern sample for ExpandableList with Object support

---
+++ pattern-expandablelist-object/src/views/MainPanel.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import kind from '@enact/core/kind';
+import ExpandableList from '@enact/moonstone/ExpandableList';
+import {Panel, Header} from '@enact/moonstone/Panels';
+
+const MainPanel = kind({
+	name: 'MainPanel',
+
+	render: (props) => (
+		<Panel {...props}>
+			<Header title={props.title} />
+			<ExpandableList
+				noneText={'nothing selected'}
+				select={'multiple'}
+				title={'ExpandableList with Data in Object format'}
+			>
+				{[{disabled:false, children: 'option1'}, {disabled:false, children: 'option2'}, {disabled:false, children: 'option3'}]}
+			</ExpandableList>
+		</Panel>
+	)
+});
+
+export default MainPanel;
