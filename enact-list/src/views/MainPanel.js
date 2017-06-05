fcbd0bc945b4cd0138523172feba121b3fe72ca5
YunBum SUNG <yb.sung@lge.com>
2017-06-05 12:53:55 +0900

    Add VirtualGridList sample
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- enact-list/src/views/MainPanel.js
+++ enact-list/src/views/MainPanel.js
@@ -3,7 +3,7 @@ import kind from '@enact/core/kind';
 import {Panel, Header} from '@enact/moonstone/Panels';
 import LocaleSwitch from '../components/LocaleSwitch';
 
-import VirtualList from '@enact/moonstone/VirtualList';
+import {VirtualList, VirtualGridList} from '@enact/moonstone/VirtualList';
 import Item from '@enact/moonstone/Item';
 
 const
@@ -14,6 +14,8 @@ const
 			position: 'absolute'
 		},
 		list: {
+			display: 'inline-block',
+			width: '50%',
 			height: '552px'
 		}
 	},
@@ -21,8 +23,17 @@ const
 	isItemDisabled = (index) => !(index % 5 === 0),
 	// eslint-disable-next-line enact/prop-types, enact/display-name
 	renderItem = ({data, index, ...rest}) => {
-		const itemSize = 72;
-		const itemStyle = {height: itemSize + 'px', ...style.item};
+		const itemStyle = {height: '72px', ...style.item};
+		const disabled = isItemDisabled(index);
+
+		return (
+			<Item {...rest} disabled={disabled} style={itemStyle}>
+				{data[index]}
+			</Item>
+		);
+	},
+	renderHorizontalItem = ({data, index, ...rest}) => {
+		const itemStyle = {width: '100px', height: '100%', overflow: 'hidden', ...style.item};
 		const disabled = isItemDisabled(index);
 
 		return (
@@ -54,6 +65,23 @@ const MainPanel = kind({
 					isItemDisabled={isItemDisabled}
 					itemSize={itemSize}
 					style={style.list}
+				/>
+				<VirtualGridList
+					component={renderItem}
+					data={items}
+					dataSize={items.length}
+					isItemDisabled={isItemDisabled}
+					itemSize={{minWidth: 100, minHeight: 100}}
+					style={style.list}
+				/>
+				<VirtualList
+					component={renderHorizontalItem}
+					data={items}
+					dataSize={items.length}
+					direction="horizontal"
+					isItemDisabled={isItemDisabled}
+					itemSize={itemSize}
+					style={{height: '200px'}}
 				/>
 			</Panel>
 		);
