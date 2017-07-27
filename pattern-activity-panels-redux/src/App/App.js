c44f18a137288b35a45efe38d4cb3202ac6dbc63
Blake Stephens <blake.stephens@lge.com>
2017-07-26 18:56:12 -0700

    Added additional converted components
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- pattern-activity-panels-redux/src/App/App.js
+++ pattern-activity-panels-redux/src/App/App.js
@@ -2,6 +2,9 @@ import kind from '@enact/core/kind';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 import React from 'react';
 import Button, {CustomButton} from '../components/MyButt';
+import BodyText from '@enact/moonstone/BodyText';
+import Divider from '@enact/moonstone/Divider';
+import Image from '@enact/moonstone/Image';
 
 const App = kind({
 	name: 'App',
@@ -9,10 +12,13 @@ const App = kind({
 	render: ({...rest}) => {
 		return (
 			<div {...rest}>
-				<Button>Uncustom button</Button>
-				<CustomButton>custom button</CustomButton>
-				<Button small color="green">Uncustom button</Button>
-				<CustomButton small color="red">custom button</CustomButton>
+				<Button casing="word">Uncustom button</Button>
+				<CustomButton casing="word">custom button</CustomButton>
+				<Button small color="green" casing="word">Uncustom button</Button>
+				<CustomButton small color="red" casing="word">custom button</CustomButton>
+				<BodyText>moonstone BodyText</BodyText>
+				<Divider>moonstone Divider</Divider>
+				<Image src="http://lorempixel.com/256/256/" sizing="fit" />
 			</div>
 		);
 	}
