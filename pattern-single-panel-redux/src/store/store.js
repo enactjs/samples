3e67d341436b6dcffbd4aa2383a4f853e57cdfdc
Roy Sutton <pre101@pre101.com>
2018-03-26 16:53:21 -0400

    Add Travis task and root level commands (#53)
    * Fix lint warnings
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Fix lint warnings
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * remove test scripts entry

--- pattern-single-panel-redux/src/store/store.js
+++ pattern-single-panel-redux/src/store/store.js
@@ -3,9 +3,9 @@ import rootReducer from '../reducers';
 
 export default function configureStore (initialState) {
 	const store = createStore(
-    rootReducer,
-    initialState
-  );
+		rootReducer,
+		initialState
+	);
 
 	if (module.hot) {
 		// Enable Webpack hot module replacement for reducers
