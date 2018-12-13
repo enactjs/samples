9da3b542e0b1269fc794fc8224e2060d8918fc5b
Ryan Duffy <ryan.duffy@lge.com>
2018-12-13 11:53:44 -0800

    ENYO-5714: Fix missing key in sample

--- pattern-expandablelist-object/src/views/MainPanel.js
+++ pattern-expandablelist-object/src/views/MainPanel.js
@@ -15,9 +15,9 @@ const MainPanel = kind({
 				title={'ExpandableList with Data in Object format'}
 			>
 				{[
-					{disabled:false, children: 'option1'},
-					{disabled:false, children: 'option2'},
-					{disabled:true, children: 'option3'}
+					{disabled:false, children: 'option1', key: 'option1'},
+					{disabled:false, children: 'option2', key: 'option2'},
+					{disabled:true, children: 'option3', key: 'option3'}
 				]}
 			</ExpandableList>
 		</Panel>
