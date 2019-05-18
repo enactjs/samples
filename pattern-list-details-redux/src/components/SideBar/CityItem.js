bbf184dd78b9ec78271f73893cfda9a768c7753b
Blake Stephens <blake.stephens@lge.com>
2019-05-17 21:11:35 -0700

    ENYO-5968: Greatly simplified pattern through refactoring
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    # Conflicts:
    #	pattern-list-details/src/components/Content.js
    #	pattern-list-details/src/components/Nav.js

--- pattern-list-details-redux/src/components/SideBar/CityItem.js
+++ pattern-list-details-redux/src/components/SideBar/CityItem.js
@@ -1,6 +1,6 @@
 import {connect} from 'react-redux';
-import Divider from '@enact/moonstone/Divider';
 import kind from '@enact/core/kind';
+import {adaptEvent, handle, forward} from '@enact/core/handle';
 import PropTypes from 'prop-types';
 import React from 'react';
 import SelectableItem from '@enact/moonstone/SelectableItem';
@@ -14,30 +14,31 @@ const CityItem = kind({
 		selected: PropTypes.bool
 	},
 
-	render: ({city, onCityChange, selected, ...rest}) => {
-		return (
-			<div {...rest}>
-				<SelectableItem
-					onToggle={onCityChange}
-					selected={selected}
-					value={city}
-				>
-					{city}
-				</SelectableItem>
-				<Divider />
-			</div>
-		);
-	}
+	handlers: {
+		onCityChange: handle(
+			adaptEvent((ev, {city}) => ({type: 'onCityChange', city}), forward('onCityChange'))
+		)
+	},
+
+	render: ({city, onCityChange, selected, ...rest}) => (
+		<SelectableItem
+			{...rest}
+			onToggle={onCityChange}
+			selected={selected}
+			value={city}
+		>
+			{city}
+		</SelectableItem>
+	)
 });
 
 const mapStateToProps = (state, ownProps) => {
 	const country = state.country;
 	const city = ownProps.city
-		.split(' ')
-		.join('')
+		.replace(/ /g, '')
 		.toLowerCase();
 
-	return city in state.data[country] ? {selected: state.data[country][city].selected} : {selected : false};
+	return city in state.data[country] ? {selected: state.data[country][city].selected, value: city} : {selected : false, value: city};
 };
 
 const CityItemContainer = connect(mapStateToProps, {})(CityItem);
--- pattern-list-details-redux/src/components/SideBar/SideBar.js
+++ pattern-list-details-redux/src/components/SideBar/SideBar.js
@@ -3,53 +3,26 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import CityItem from './CityItem';
-import css from './SideBar.module.less';
 
 const SideBar = kind({
 	name: 'SideBar',
 
 	propTypes: {
 		cities: PropTypes.array.isRequired,
-		onCityChange: PropTypes.func.isRequired,
-		zoom: PropTypes.bool
+		onCityChange: PropTypes.func.isRequired
 	},
 
-	styles: {
-		css,
-		className: 'sideBar'
-	},
-
-	computed: {
-		className: ({zoom, styler}) => {
-			return styler.append(css.sideBar, {zoom});
-		},
-		cityList: ({cities, onCityChange, ...rest}) => {
-			delete rest.zoom;
-
-			return cities.map((city, index) => {
-				return (
-					<CityItem
-						{...rest}
-						city={city}
-						key={index}
-						onCityChange={onCityChange}
-					/>
-				);
-			});
-		}
-	},
-
-	render: ({cityList, ...rest}) => {
-		delete rest.cities;
-		delete rest.onCityChange;
-		delete rest.zoom;
-
-		return (
-			<div {...rest}>
-				{cityList}
-			</div>
-		);
-	}
+	render: ({cities, onCityChange}) => (
+		<React.Fragment>
+			{cities.map((city, index) => (
+				<CityItem
+					city={city}
+					key={'cityItem' + index}
+					onCityChange={onCityChange}
+				/>
+			))}
+		</React.Fragment>
+	)
 });
 
 export default SideBar;
