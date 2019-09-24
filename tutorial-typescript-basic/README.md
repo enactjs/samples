d39d12a25e13204c742b7f9a3eabc13a992ceb4d
Renuka Atale <renuka.atale@lge.com>
2019-09-24 14:29:51 -0700

    update type for callback functions, add link in tutorial and package json

--- tutorial-typescript-basic/README.md
+++ tutorial-typescript-basic/README.md
@@ -1,3 +1,9 @@
 The completed counter app from the TypeScript Enact tutorial.
 
 Run `npm install` then `npm run serve` to have the app running on [http://localhost:8080](http://localhost:8080), where you can view it in your browser.
+
+Complete documentation related to tutorial implemetation is available at (https://enactjs.com/docs/tutorials/tutorial-typescript-enact)
+
+---
+
+This project was bootstrapped with the Enact [cli](https://github.com/enactjs/cli).
--- tutorial-typescript-basic/src/components/Counter/Counter.tsx
+++ tutorial-typescript-basic/src/components/Counter/Counter.tsx
@@ -5,17 +5,17 @@ import kind from '@enact/core/kind';
 import PropTypes, { number } from 'prop-types';
 import React from 'react';
 
-//Using the type keyword
-type MyFunctionType = (count: number) => number;
 interface counterProps {
     count? : number,
-    onResetClick? : MyFunctionType,
-    onCounterChange? : MyFunctionType,
-    onDecrementClick? : MyFunctionType,
-    onIncrementClick? : MyFunctionType
+    onResetClick? : void,
+    onCounterChange? : void,
+    onDecrementClick? : void,
+    onIncrementClick? : void
 }
 
-function createHandler(fn:MyFunctionType) {
+type handlerFunctionType = (count: number) => number;
+
+function createHandler(fn:handlerFunctionType) {
     return handle(
         adaptEvent((ev, {count}) => ({
             type: 'onCounterChange',
@@ -29,22 +29,14 @@ function createHandler(fn:MyFunctionType) {
 const CounterBase = kind<counterProps>({
 	name: 'Counter',
 
-	propTypes: {
-        count: PropTypes.number,
-        onCounterChange: PropTypes.func,
-        onIncrementClick: PropTypes.func,
-        onDecrementClick: PropTypes.func,
-        onResetClick: PropTypes.func
-	},
-
 	defaultProps: {
         count: 0
     },
 
     handlers: {
-        'onDecrementClick': createHandler(count => count - 1),
-        'onIncrementClick': createHandler(count => count + 1),
-        'onResetClick': createHandler(() => 0)
+        onDecrementClick: createHandler(count => count - 1),
+        onIncrementClick: createHandler(count => count + 1),
+        onResetClick: createHandler(() => 0)
 	},
 
 	render: ({onIncrementClick, onDecrementClick, onResetClick, count, ...rest}) => (
