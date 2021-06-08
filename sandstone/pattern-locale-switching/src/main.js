5e6861b248805ebe741a1f30f0661e8579a931ee
Juwon Jeong <juwon.jeong@lge.com>
2021-06-08 09:16:30 +0900

    Move sandstone related pattern samples to sandstone folder
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

---
+++ sandstone/pattern-locale-switching/src/main.js
@@ -0,0 +1,15 @@
+import {Provider} from 'react-redux';
+
+import App from './App';
+import configureStore from './store';
+
+// set default launch path
+const store = configureStore();
+
+let appElement = () => (
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
+
+export default appElement;
---
+++ sandstone/pattern-single-panel-redux/src/containers/PhotoPreviewContainer.js
@@ -0,0 +1,43 @@
+import kind from '@enact/core/kind';
+import Image from '@enact/moonstone/Image';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import css from './containerStyles.module.less';
+
+const PhotoPreview = kind({
+	name: 'PhotoPreview',
+
+	propTypes: {
+		imageURLs: PropTypes.array.isRequired,
+		photoIndex: PropTypes.number.isRequired,
+		photoPosition: PropTypes.number.isRequired,
+		dispatch: PropTypes.func
+	},
+
+	styles: {
+		css,
+		className: 'profilePhoto'
+	},
+
+	render: ({imageURLs, photoIndex, photoPosition, ...rest}) => {
+		delete rest.dispatch;
+
+		return (
+			<Image {...rest} src={imageURLs[photoIndex]} style={{backgroundPosition: photoPosition + 'px'}} />
+		);
+	}
+});
+
+const mapStateToProps = (state) => {
+	return ({
+		photoIndex: state.photoIndex,
+		photoPosition: state.photoPosition
+	});
+};
+
+const PhotoPreviewContainer = connect(
+	mapStateToProps
+)(PhotoPreview);
+
+export default PhotoPreviewContainer;
