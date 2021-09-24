2bc2d135f3dce7bbabe1bac5aeee46bf9fcaa5e8
adrian-cocoara-lgp <adrian.cocoara@lgepartner.com>
2021-09-24 14:45:49 +0300

    merge with feature/WRN-7349

--- sandstone/custom-skin/src/utils.js
+++ sandstone/custom-skin/src/utils.js
@@ -1,16 +1,16 @@
-const getRandomColor = (color) => {
-	let p = 1,
-		temp,
-		random = Math.random(),
-		result = '#';
-
-	while (p < color.length) {
-		temp = parseInt(color.slice(p, p += 2), 16);
-		temp += Math.floor((255 - temp) * random);
-		result += temp.toString(16).padStart(2, '0');
-	}
-	return result;
-};
+// const getRandomColor = (color) => {
+// 	let p = 1,
+// 		temp,
+// 		random = Math.random(),
+// 		result = '#';
+//
+// 	while (p < color.length) {
+// 		temp = parseInt(color.slice(p, p += 2), 16);
+// 		temp += Math.floor((255 - temp) * random);
+// 		result += temp.toString(16).padStart(2, '0');
+// 	}
+// 	return result;
+// };
 
 const convertHexToRGB = (hex) => {
 	let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
