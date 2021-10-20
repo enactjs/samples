e9862057747112ae402bbb2eca17d49804655cb6
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-10-20 14:41:37 +0300

    Added if statement to check the requests.

--- sandstone/custom-skin/file-generator/index.js
+++ sandstone/custom-skin/file-generator/index.js
@@ -5,26 +5,31 @@ const port = 5000;
 const server = http.createServer((req, res) => {
 	const body = [];
 
-	req.on('data', (chunk) => {
-		body.push(chunk);
-	});
-
-	req.on('end', () => {
-		// eslint-disable-next-line
-		const finalBody = Buffer.concat(body)?.toString()?.split(':')[1]?.split('[')[1]?.split(']}')[0];
-		let colors = finalBody.split(',');
-		colors = colors.map(color => {
-			if (color.length > 5) {
-				return color.split('"')[1];
-			} else {
-				return color;
-			}
+	if (req.method === 'POST' && req.url === '/write') {
+		req.on('data', (chunk) => {
+			body.push(chunk);
 		});
-		writeToFile(colors);
-	});
 
-	res.setHeader('access-control-allow-origin', '*');
-	res.statusCode = 200;
+		req.on('end', () => {
+			// eslint-disable-next-line
+			const finalBody = Buffer.concat(body)?.toString()?.split(':')[1]?.split('[')[1]?.split(']}')[0];
+			let colors = finalBody.split(',');
+			colors = colors.map(color => {
+				if (color.length > 5) {
+					return color.split('"')[1];
+				} else {
+					return color;
+				}
+			});
+			writeToFile(colors);
+		});
+
+		res.setHeader('access-control-allow-origin', '*');
+		res.statusCode = 200;
+	} else {
+		res.setHeader('access-control-allow-origin', '*');
+		res.statusCode = 400;
+	}
 	res.end();
 });
 
