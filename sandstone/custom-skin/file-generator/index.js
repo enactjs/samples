9ac2bedfd62e5b8b177148319df697e2caa45ad1
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-10-20 11:13:55 +0300

    Created a live demo.

---
+++ sandstone/custom-skin/file-generator/index.js
@@ -0,0 +1,30 @@
+const {writeToFile} = require('./writeToFile');
+const http = require("http");
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+	const body = [];
+
+	req.on('data', (chunk) => {
+		body.push(chunk);
+	});
+
+	req.on('end', () => {
+		const finalBody = Buffer.concat(body)?.toString()?.split(':')[1]?.split('[')[1]?.split(']}')[0];
+		let colors = finalBody.split(',');
+		colors = colors.map(color => {
+			if(color.length > 5){
+				return color.split('"')[1];
+			}
+			else {
+				return color;
+			}
+		});
+		writeToFile(colors);
+	});
+
+	res.statusCode = 202;
+	res.end();
+});
+
+server.listen(port);
