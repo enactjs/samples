a8021408c23141b76d3f778217717783eaf8f013
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-02-09 10:02:25 +0200

    code review fixes

--- sandstone/custom-skin/src/components/OutputField/OutputField.js
+++ sandstone/custom-skin/src/components/OutputField/OutputField.js
@@ -45,7 +45,7 @@ const OutputField = kind({
 
 		return (
 			<Cell size="90%" className={css.outputField}>
-				<Popup open={popupOpen} onClose={onToggleOpen}>
+				<Popup open={popupOpen} onClose={onToggleOpen} spotlightRestrict="self-only">
 					<Scroller>
 						<pre className={css.outputData}>
 							{text}
@@ -53,7 +53,7 @@ const OutputField = kind({
 					</Scroller>
 				</Popup>
 				<div className={css.outputBtnContainer}>
-					<TooltipButton className={css.outputBtn} icon="folder" minWidth={false} onClick={onToggleOpen} size="small" tooltipText="Copy to clipboard">Show output</TooltipButton>
+					<TooltipButton className={css.outputBtn} icon="folder" minWidth={false} onClick={onToggleOpen} size="small" tooltipText="Show output data">Show output</TooltipButton>
 					<TooltipButton className={css.outputBtn} css={css} icon="files" minWidth={false} onClick={copyToClipboard} size="small" tooltipText="Copy to clipboard">Copy</TooltipButton>
 					<TooltipButton className={css.outputBtn} css={css} icon="download" minWidth={false} onClick={generateFile} size="small" tooltipText="Get CSS file">Download</TooltipButton>
 					<TooltipButton className={css.outputBtn} css={css} icon="refresh" minWidth={false} onClick={setDefaultState} size="small" tooltipText="Restore skin to default colors">Reset</TooltipButton> {/* eslint-disable-line */}
