046adb12de6a999851d285c19027f24e853db241
sj.ro@lge.com <sj.ro@lge.com>
2022-02-21 12:55:08 +0900

    Add tutorial samples to moonstone directory Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)

---
+++ moonstone/tutorial-kitten-browser/src/index.js
@@ -0,0 +1,13 @@
+import {render} from 'react-dom';
+
+import App from './App';
+
+const appElement = (<App />);
+
+// In a browser environment, render the app to the document.
+if (typeof window !== 'undefined') {
+	// 'root' div is provided by Enact's HTML template
+	render(appElement, document.getElementById('root'));
+}
+
+export default appElement;
