bd41ca6268df35a7a18b3ebf1ede007ea6676ac9
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-03-07 17:40:25 +0200

    added sampler for useTransition

--- sandstone/react-18/src/views/UseTransition/UseTransition.js
+++ sandstone/react-18/src/views/UseTransition/UseTransition.js
@@ -35,7 +35,9 @@ const UseTransition = () => {
 		<Scroller>
 			<div>
 				<BodyText className={css.guideText} size="small">
-					Until React 17, when needing to fetch data before showing some UI that depends on that data, a loading state would have been rendered in its place, for example a spinner, until the request resolved.
+					Until React 17, when needing to fetch data before showing some UI that depends on that data, a loading state would have been rendered in its place.
+					<br />
+					When one of the buttons is clicked, the 'Content' component fetches some data. While that data is fetched, a 'Spinner' is loaded in the content's place.
 				</BodyText>
 				<div className={css.demoContainer}>
 					<Group
@@ -48,17 +50,14 @@ const UseTransition = () => {
 					>
 						{['One', 'Two', 'Three']}
 					</Group>
-					<div>
+					<div className={css.content}>
 						{tab === 0 && <Content page="One" />}
 						{tab === 1 && <Content page="Two" />}
 						{tab === 2 && <Content page="Three" />}
 					</div>
 				</div>
 				<BodyText className={css.guideText} size="small">
-					Every time the data is changed, the 'Content' component for that tab fetches some data. While that data is being fetched, a 'Spinner' is loaded in the content's place.
-				</BodyText>
-				<BodyText className={css.guideText} size="small">
-					With React18's 'useTransition' hook, the previous state of the UI can be held until the data is ready in a straight-forward way.
+					With React18's 'useTransition' hook, the previous state of the UI can be held until the data is ready. The fetching of the new data is wrapped inside startTransition while the isPending data tells if the content is currently being loaded or not. This allows the possibility to show a loading indicator. Its timeoutMs property specifies how long we’re willing to wait for the transition to finish.
 				</BodyText>
 				<div className={css.demoContainer}>
 					<Group
@@ -86,9 +85,6 @@ const UseTransition = () => {
 						</div>
 					</div>
 				</div>
-				<BodyText className={css.guideText} size="small">
-					Now instead of switching tabs immediately, the current tab continues to show its content until the new tab's content is ready. There is also the possibility to show a loading indicator, by making use of the 'isPending' prop of useTransition
-				</BodyText>
 			</div>
 		</Scroller>
 	);
