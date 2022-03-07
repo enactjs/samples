6e0d5f8cbaa50f85b81715ddf862d94afcb05a95
Alex Morariu <alexandru.morariu@lgepartner.com>
2022-03-07 16:05:47 +0200

    Created the batching example.

--- sandstone/react-18/src/views/Batching/Batching.js
+++ sandstone/react-18/src/views/Batching/Batching.js
@@ -1,6 +1,12 @@
+/* eslint-disable react-hooks/rules-of-hooks, react/jsx-no-bind */
+
 import kind from '@enact/core/kind';
-import Button from '@enact/sandstone/Button';
+import {Cell, Row} from '@enact/ui/Layout';
+import BodyText from '@enact/sandstone/BodyText';
 import {Panel, Header} from '@enact/sandstone/Panels';
+
+import BatchedAssign from './BatchedAssign';
+import NotBatchedAssign from './NotBatchedAssign';
 
 const Batching = kind({
 	name: 'Batching',
@@ -8,7 +14,23 @@ const Batching = kind({
 	render: (props) => (
 		<Panel {...props}>
 			<Header title="Batching" type="mini" />
-			<Button>Click me</Button>
+			<Row>
+				<Cell>
+					<BodyText>
+						Batching is when React groups multiple state updates into a single re-render for better performance.
+					</BodyText>
+					<BodyText>
+						In this example we have a For Loop that will call a state updating function a 1000 times incrementing
+						or decrementing the value by 1 each time.
+					</BodyText>
+				</Cell>
+				<Cell>
+					<BatchedAssign />
+				</Cell>
+				<Cell>
+					<NotBatchedAssign />
+				</Cell>
+			</Row>
 		</Panel>
 	)
 });
---
+++ sandstone/react-18/src/views/Batching/NotBatchedAssign.js
@@ -0,0 +1,54 @@
+/* eslint-disable react-hooks/rules-of-hooks, react/jsx-no-bind */
+
+import kind from '@enact/core/kind';
+import {Column} from '@enact/ui/Layout';
+import BodyText from '@enact/sandstone/BodyText';
+import Button from '@enact/sandstone/Button';
+import {useEffect, useState} from 'react';
+import {flushSync} from 'react-dom';
+
+const NotBatchedAssign = kind({
+	name: 'BatchedAssign',
+
+	functional: true,
+
+	render: () => {
+		const [value, setValue] = useState(0);
+		const [renders, setRenders] = useState(-1);
+
+		useEffect(() => {
+			setRenders(r => r + 1);
+		}, [value]);
+
+		const assignFunctionNotBatched = () => {
+			if (value === 0) {
+				for (let i = 0; i < 1000; ++i) {
+					flushSync(() => {
+						setValue(i + 1);
+					});
+				}
+			} else {
+				for (let i = 1000; i >= 0; --i) {
+					flushSync(() => {
+						setValue(i);
+					});
+				}
+			}
+		};
+
+		return (
+			<div>
+				<Column>
+					<BodyText>If you click `Not Batched update!` the value will go from {value} to {value === 0 ? 1000 : 0}!</BodyText>
+					<BodyText>The current value is {value}!</BodyText>
+					<BodyText>There have been {renders} re-renders!</BodyText>
+					<Button onClick={() => assignFunctionNotBatched(value)}>Not Batched update!</Button>
+					<Button onClick={() => setRenders(0)}>Clear re-renders!</Button>
+				</Column>
+			</div>
+		);
+	}
+});
+
+export default NotBatchedAssign;
+
