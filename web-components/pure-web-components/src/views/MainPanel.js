be974819e5cd709d5393331a889bbe29dd3ea8a3
Seungcheon Baek <sc.baek@lge.com>
2022-04-06 15:41:48 +0900

    WRN-19651: POC of pure web components
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- web-components/pure-web-components/src/views/MainPanel.js
+++ web-components/pure-web-components/src/views/MainPanel.js
@@ -2,8 +2,8 @@ import kind from '@enact/core/kind';
 import Heading from '@enact/sandstone/Heading';
 import {Panel, Header} from '@enact/sandstone/Panels';
 
-import {SimpleButton, SpottableButton} from '../components/Button';
-import css from '../components/Button/Button.module.less';
+import {ButtonSimple, ButtonWithIcon, SpottableButtonSimple, SpottableButtonWithIcon} from '../components/Button';
+import css from './MainPanel.module.less';
 
 const rest = {};
 
@@ -14,43 +14,55 @@ const MainPanel = kind({
 		<Panel {...props}>
 			<Header title="Hello world!" />
 			<>
-				<Heading>DOM element</Heading>
-
-				A plain text
-				<button>The standard button</button>
-
 				<Heading>Using poc-button-simple</Heading>
-
-				<poc-button-simple><button>With the standard button</button></poc-button-simple>
-				<poc-button-simple>With a text</poc-button-simple>
-				<poc-button-simple class={css.button}>With a text and size</poc-button-simple>
-				<poc-button-simple class={css.button} {...rest}>
-					<div slot="background" className={css.background} />
-					<span slot="icon">🍭</span>
-					<span>Click Me</span>
-				</poc-button-simple>
+				<div className={css.section}>
+					<poc-button-simple><button>With the standard button</button></poc-button-simple>
+					<poc-button-simple>With a text</poc-button-simple>
+					<poc-button-simple class={css.button}>With a text and size</poc-button-simple>
+					<poc-button-simple class={css.button} {...rest}>
+						<div slot="background" className={css.background} />
+						<span slot="icon">🍭</span>
+						<span>Click Me</span>
+					</poc-button-simple>
+				</div>
 
 				<Heading>Using poc-button-with-icon</Heading>
+				<div className={css.section}>
+					<poc-button-with-icon><button>With the standard button</button></poc-button-with-icon>
+					<poc-button-with-icon>With a text</poc-button-with-icon>
+					<poc-button-with-icon class={css.button}>With a text and size</poc-button-with-icon>
+					<poc-button-with-icon class={css.button} {...rest}>
+						<div id="target" slot="background" className={css.background} />
+						<span slot="icon">🍭</span>
+						<span>Click Me</span>
+					</poc-button-with-icon>
+				</div>
 
-				<poc-button-with-icon><button>With the standard button</button></poc-button-with-icon>
-				<poc-button-with-icon>With a text</poc-button-with-icon>
-				<poc-button-with-icon class={css.button}>With a text and size</poc-button-with-icon>
-				<poc-button-with-icon class={css.button} {...rest}>
-					<div id="target" slot="background" className={css.background} />
-					<span slot="icon">🍭</span>
-					<span>Click Me</span>
-				</poc-button-with-icon>
+				<Heading>Using ButtonSimple</Heading>
+				<div className={css.section}>
+					<ButtonSimple><button>With the standard button</button></ButtonSimple>
+					<ButtonSimple>With a text</ButtonSimple>
+				</div>
 
-				<Heading>Using SimpleButton</Heading>
+				<Heading>Using ButtonWithIcon</Heading>
+				<div className={css.section}>
+					<ButtonWithIcon>With a text</ButtonWithIcon>
+					<ButtonWithIcon icon={'🍭'} />
+					<ButtonWithIcon icon={'🍭'}>With a text</ButtonWithIcon>
+				</div>
 
-				<SimpleButton><button>With the standard button</button></SimpleButton>
-				<SimpleButton>With a text</SimpleButton>
+				<Heading>Using SpottableButtonSimple</Heading>
+				<div className={css.section}>
+					<SpottableButtonSimple><button>With the standard button</button></SpottableButtonSimple>
+					<SpottableButtonSimple>With a text</SpottableButtonSimple>
+				</div>
 
-				<Heading>Using SpottableButton</Heading>
-
-				<SpottableButton>With a text</SpottableButton>
-				<SpottableButton icon={'🍭'} />
-				<SpottableButton icon={'🍭'}>With a text</SpottableButton>
+				<Heading>Using SpottableButtonWithIcon</Heading>
+				<div className={css.section}>
+					<SpottableButtonWithIcon>With a text</SpottableButtonWithIcon>
+					<SpottableButtonWithIcon icon={'🍭'} />
+					<SpottableButtonWithIcon icon={'🍭'}>With a text</SpottableButtonWithIcon>
+				</div>
 			</>
 		</Panel>
 	)
