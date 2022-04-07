516b114c312afa75162a8717f0764208aa80bd92
Seungcheon Baek <sc.baek@lge.com>
2022-04-07 14:09:25 +0900

    Use lit
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- web-components/pure-web-components/src/components/Button/Button.js
+++ web-components/pure-web-components/src/components/Button/Button.js
@@ -3,142 +3,86 @@
 import Spottable from '@enact/spotlight/Spottable';
 import Touchable from '@enact/ui/Touchable';
 import classNames from 'classnames';
+import {LitElement, html, css} from 'lit';
 
-import css from './Button.module.less';
+import componentCss from './Button.module.less';
 
-/* This is a sample for a new custom element
-Reference: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
-customElements.define('element-name-here', // a name should be kebab-case and cannot be a single word
-	class extends HTMLElement {
-		constructor () {
-			// For the prototype chain
-			super();
-			// Set the shadow root
-			const shadowRoot = this.attachShadow({mode: 'open'});
+
+class PocButtonSimple extends LitElement {
+	static styles = [css`
+		:host {
+			position: relative;
+			display: inline-block;
+			vertical-align: middle;
+			text-align: center;
+			z-index: 0;
 		}
-		static get observedAttributes () { // if we don't have this getter, attributeChangedCallback will be called for any attribute
-			return ['attribute-name-here'];
+		.root {
+			height: 100%;
+			width: 100%;
 		}
-		connectedCallback () { // when an element is added to DOM tree
-			console.log('Custom square element added to page.');
-			updateStyle(this);
+		.background::slotted(*) {
+			position: absolute;
+			display: inline-block;
+			height: 100%;
+			width: 100%;
+			inset: 0;
+			z-index: -1;
 		}
-		disconnectedCallback () { // when an element is removed from DOM tree
-			console.log('Custom square element removed from page.');
+		.content {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100%;
 		}
-		adoptedCallback () { // when an element is moved to another DOM tree
-			console.log('Custom square element moved to new page.');
-		}
-		attributeChangedCallback (name, oldValue, newValue) { // when an element's attribute is changed
-			console.log('Custom square element attributes changed.');
-			updateStyle(this);
-		}
+	`];
+	render () {
+		return html`
+			<div class="root">
+				<slot name="background" class="background"></slot>
+				<div class="content"><slot></slot></div>
+			</div>
+		`;
 	}
-);
-*/
+}
+customElements.define('poc-button-simple', PocButtonSimple);
 
-
-/* Define Web Component */
-customElements.define('poc-button-simple',
-	class extends HTMLElement {
-		constructor () {
-			// For the prototype chain
-			super();
-
-			// Set the shadow root
-			const shadowRoot = this.attachShadow({mode: 'open'});
-
-			/*
-			shadowRoot.applyAuthorStyles = true;
-			shadowRoot.resetStyleInheritance = true;
-			*/
-
-			shadowRoot.innerHTML = `
-				<style>
-					:host {
-						position: relative;
-						display: inline-block;
-						vertical-align: middle;
-						text-align: center;
-						z-index: 0;
-					}
-					.root {
-						height: 100%;
-						width: 100%;
-					}
-					.background::slotted(*) {
-						position: absolute;
-						display: inline-block;
-						height: 100%;
-						width: 100%;
-						inset: 0;
-						z-index: -1;
-					}
-					.content {
-						display: flex;
-						justify-content: center;
-						align-items: center;
-						height: 100%;
-					}
-				</style>
-				<div class="root">
-					<slot name="background" class="background"></slot>
-					<div class="content"><slot></slot></div>
-				</div>
-			`;
+class PocButtonWithIcon extends LitElement {
+	static styles = [css`
+		:host {
+			position: relative;
+			display: inline-block;
+			vertical-align: middle;
+			z-index: 0;
 		}
-	}
-);
-
-/* Define Web Component */
-customElements.define('poc-button-with-icon',
-	class extends HTMLElement {
-		constructor () {
-			// For the prototype chain
-			super();
-
-			// Set the shadow root
-			const shadowRoot = this.attachShadow({mode: 'open'});
-
-			/*
-			shadowRoot.applyAuthorStyles = true;
-			shadowRoot.resetStyleInheritance = true;
-			*/
-
-			shadowRoot.innerHTML = `
-				<style>
-					:host {
-						position: relative;
-						display: inline-block;
-						vertical-align: middle;
-						z-index: 0;
-					}
-					:host .background {} /* not working */
-					.root {
-						height: 100%;
-						width: 100%;
-					}
-					.background::slotted(*:hover) {} /* not working */
-					.background::slotted(*:focus) {} /* not working */
-				</style>
-				<poc-button-simple class="root">
-					<slot name="background" slot="background" class="background"></slot>
-					<slot name="icon"></slot>
-					<slot></slot>
-				</poc-button-simple>
-			`;
+		:host .background {} /* not working */
+		.root {
+			height: 100%;
+			width: 100%;
 		}
+		.background::slotted(*:hover) {} /* not working */
+		.background::slotted(*:focus) {} /* not working */
+	`];
+	render () {
+		return html`
+			<poc-button-simple class="root">
+				<slot name="background" slot="background" class="background"></slot>
+				<slot name="icon"></slot>
+				<slot></slot>
+			</poc-button-simple>
+		`;
 	}
-);
+};
+customElements.define('poc-button-with-icon', PocButtonWithIcon);
 
 const ButtonSimple = (props) => (
 	<poc-button-simple {...props} />
 );
 
 const ButtonWithIcon = ({children, className, icon, pressed, ...rest}) => (
-	<poc-button-with-icon class={classNames(css.button, className, pressed ? 'pressed' : '')} {...rest}>
-		<div id="target" slot="background" className={css.background} />
-		<span slot="icon" className={css.icon}>{icon}</span>
+	<poc-button-with-icon class={classNames(componentCss.button, className, pressed ? 'pressed' : '')} {...rest}>
+		<div id="target" slot="background" className={componentCss.background} />
+		<span slot="icon" className={componentCss.icon}>{icon}</span>
 		{children}
 	</poc-button-with-icon>
 );
