ff7309c95a97c8c071d4b7c228c199c14600e775
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-04-08 11:47:51 +0300

    added figma plugin

---
+++ sandstone/figma-plugin/src/ui.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import './ui.css';
+import Button from '@enact/sandstone/Button';
+
+declare function require(path: string): any
+
+class App extends React.Component {
+    textbox: HTMLInputElement
+
+    countRef = (element: HTMLInputElement) => {
+        if (element) element.value = '5'
+        this.textbox = element
+    }
+
+    onCreate = () => {
+        const count = parseInt(this.textbox.value, 10)
+        parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
+    }
+
+    onCancel = () => {
+        parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+    }
+
+    render() {
+        return <div>
+            <img src={require('./logo.svg')} />
+            <h2>Rectangle Creator</h2>
+            <p>Count: <input ref={this.countRef} /></p>
+            <button id="create" onClick={this.onCreate}>Create</button>
+            <button onClick={this.onCancel}>Cancel</button>
+            <Button>Hello</Button>
+        </div>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('react-page'))
