5cf3714d4a096883dc06a15f601dbd2d0e22f71a
Stanca Pop <stanca.pop@lgepartner.com>
2022-05-17 11:28:42 +0300

    Refactored Suspense sample to work inside all-samples

---
+++ sandstone/pattern-react18-new/src/views/Suspense/NoSuspense.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+
+import SamplePage from './SamplePage';
+
+const NoSuspense = () => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 3000);
+	}, []);
+
+	return (
+		<div>
+			{
+				loading ? null :
+					<div>
+						<SamplePage/>
+					</div>
+			}
+		</div>
+	)
+}
+
+export default NoSuspense;
--- sandstone/pattern-react18-new/src/views/Suspense/Suspense.js
+++ sandstone/pattern-react18-new/src/views/Suspense/Suspense.js
@@ -1,8 +1,10 @@
-import React, {Suspense, useEffect, useState} from 'react';
-import Button from '@enact/sandstone/Button';
-import {Header, Panel, Panels} from '@enact/sandstone/Panels';
+import React, {Suspense, useState} from 'react';
+import Heading from '@enact/sandstone/Heading';
+import TabLayout, {Tab} from '@enact/sandstone/TabLayout';
 
 import SkeletonPage from './SkeletonPage';
+import NoSuspense from './NoSuspense';
+import css from "../../App/App.module.less";
 
 // Load the "SamplePage" component after 3s
 const getSamplePage = () => {
@@ -20,31 +22,28 @@ let SamplePage = getSamplePage();
 const SuspensePage = () => {
 	const [index, setIndex] = useState(0);
 
-	useEffect( () => {
-		// Lazy reload the "SamplePage" component when index changes
+	const lazyReload = ({index}) => {
 		SamplePage = getSamplePage();
-	}, [index]);
-
-	const nextPanel = () => setIndex(index + 1); /* eslint-disable react/jsx-no-bind */
-	const prevPanel = () => setIndex(index - 1); /* eslint-disable react/jsx-no-bind */
+		setIndex(index);
+	};
 
 	return (
-		<Panels index={index} noCloseButton onBack={prevPanel}>
-			<Panel>
-				<Header subtitle="Suspense offers a fallback UI for better user experience. Check out next panel." type="mini">
-					<slotAfter>
-						<Button size="small" icon="arrowlargeright" onClick={nextPanel} />
-					</slotAfter>
-				</Header>
+		<TabLayout index={index} onSelect={lazyReload} orientation="horizontal">
+			<Tab title="Using Suspense">
+				<Heading>
+					Suspense offers a fallback UI for better user experience.
+				</Heading>
 				<Suspense fallback={<SkeletonPage />}>
 					<SamplePage />
 				</Suspense>
-			</Panel>
-			<Panel>
-				<Header subtitle="Page is empty until all the data is available. Please wait 3s." type="mini" />
-				<SamplePage />
-			</Panel>
-		</Panels>
+			</Tab>
+			<Tab title="NOT Using Suspense">
+				<Heading>
+					Page is empty until all the data is available. Please wait 3s.
+				</Heading>
+				<NoSuspense />
+			</Tab>
+		</TabLayout>
 	);
 };
 
