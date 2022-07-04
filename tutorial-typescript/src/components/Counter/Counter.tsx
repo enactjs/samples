6caedb7ffcf5a1465df1fc4a910faa2b326b9e3e
vladut-clapou-lgp <96175690+vladut-clapou-lgp@users.noreply.github.com>
2022-07-04 07:26:34 +0300

    Feature/WRO-713: TypeScript: Example code of tutorial typescript is not working (#176)
    * Code sync with docs repo
    
    * Lint fixes
    
    * Review fix
    
    * Review fix

--- tutorial-typescript/src/components/Counter/Counter.tsx
+++ tutorial-typescript/src/components/Counter/Counter.tsx
@@ -2,6 +2,7 @@ import {adaptEvent, forward, handle} from '@enact/core/handle';
 import kind from '@enact/core/kind';
 import Button from '@enact/sandstone/Button';
 import Changeable from '@enact/ui/Changeable';
+import PropTypes from 'prop-types';
 
 interface Props {
 	count? : number,
@@ -10,20 +11,21 @@ interface Props {
 
 type HandlerFunctionType = (count: number) => number;
 
-function createHandler(fn: HandlerFunctionType) {
+const createHandler = (fn: HandlerFunctionType) => {
 	return handle(
-		adaptEvent(
-			(ev, {count}) => ({
-				type: 'onCounterChange',
-				count: fn(count)
-			}),
-			forward('onCounterChange')
-		)
-	)
-}
+		adaptEvent((ev, {count}) => ({
+			type: 'onCounterChange',
+			count: fn(count)
+		}), forward('onCounterChange'))
+	);
+};
 
 const CounterBase = kind<Props>({
 	name: 'Counter',
+
+	propTypes: {
+		count: PropTypes.number
+	},
 
 	defaultProps: {
 		count: 0
@@ -35,17 +37,20 @@ const CounterBase = kind<Props>({
 		onResetClick: createHandler(() => 0)
 	},
 
-	render: ({onIncrementClick, onDecrementClick, onResetClick, count, ...rest}) => (
-		<div>
-			<h1>{count}</h1>
-			<Button onClick={onDecrementClick}>Decrement --</Button>
-			<Button onClick={onResetClick}>Reset</Button>
-			<Button onClick={onIncrementClick}>Increment ++</Button>
-		</div>
-	)
+	render: ({onIncrementClick, onDecrementClick, onResetClick, count, ...rest}) => {
+		delete rest.onCounterChange;
+		return (
+			<div {...rest}>
+				<h1>{count}</h1>
+				<Button onClick={onDecrementClick}>Decrement --</Button>
+				<Button onClick={onResetClick}>Reset</Button>
+				<Button onClick={onIncrementClick}>Increment ++</Button>
+			</div>
+		)
+	}
 });
 
-const Counter = Changeable({prop: 'count' , change: 'onCounterChange'}, CounterBase);
+const Counter = Changeable({prop: 'count', change: 'onCounterChange'}, CounterBase);
 
 export default Counter;
 export {
--- tutorial-typescript/src/views/MainPanel.tsx
+++ tutorial-typescript/src/views/MainPanel.tsx
@@ -1,8 +1,8 @@
 import kind from '@enact/core/kind';
 import {Header, Panel} from '@enact/sandstone/Panels';
 
-//Custom component
-import Counter from '../components/Counter'
+// Custom component
+import Counter from '../components/Counter';
 
 const MainPanel = kind({
 	name: 'MainPanel',
