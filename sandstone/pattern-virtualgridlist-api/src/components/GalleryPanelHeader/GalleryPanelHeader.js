49a1aebc7032fee578219513a4ea40e18a57ea00
SJ RO <sj.ro@lge.com>
2022-09-02 17:15:02 +0900

    Change the image src to svg Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)

--- sandstone/pattern-virtualgridlist-api/src/components/GalleryPanelHeader/GalleryPanelHeader.js
+++ sandstone/pattern-virtualgridlist-api/src/components/GalleryPanelHeader/GalleryPanelHeader.js
@@ -11,6 +11,10 @@ import {
 	selectionEnable as selectionEnableAction
 } from '../../store';
 
+const svgGenerator = (width, height, bgColor, textColor, customText) => (
+    `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} ${height}' class='img-fluid rounded mx-auto d-block' width='${width}' height='${height}'%3E%3Crect width='${width}' height='${height}' fill='%23${bgColor}'%3E%3C/rect%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23${textColor}'%3E${customText}%3C/text%3E%3C/svg%3E`
+);
+
 const createMockItem = (album, albumSize, showOverlay) => {
 	const
 		dataLength = albumSize,
@@ -23,7 +27,7 @@ const createMockItem = (album, albumSize, showOverlay) => {
 		selectionOverlayShowing: showOverlay,
 		caption: album + ' ' + dataLength + caption,
 		subCaption: subCaption,
-		source: 'http://via.placeholder.com/300x300/' + color + '/ffffff/png?text=Image ' + dataLength
+		source: svgGenerator(300, 300, color, 'ffffff', `Image ${dataLength}`)
 	};
 };
 
--- sandstone/pattern-virtualgridlist-api/src/store/index.js
+++ sandstone/pattern-virtualgridlist-api/src/store/index.js
@@ -1,5 +1,9 @@
 import {configureStore, combineReducers, createSlice} from '@reduxjs/toolkit';
 
+const svgGenerator = (width, height, bgColor, textColor, customText) => (
+    `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} ${height}' class='img-fluid rounded mx-auto d-block' width='${width}' height='${height}'%3E%3Crect width='${width}' height='${height}' fill='%23${bgColor}'%3E%3C/rect%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23${textColor}'%3E${customText}%3C/text%3E%3C/svg%3E`
+);
+
 const createRecords = (album) => {
 	let records = {
 		album: album,
@@ -21,7 +25,7 @@ const createRecords = (album) => {
 			selectionOverlayShowing: false,
 			caption: album + ' ' + idx + caption,
 			subCaption: subCaption,
-			source: 'http://via.placeholder.com/300x300/' + color + '/ffffff&text=Image ' + idx
+			source: svgGenerator(300, 300, color, 'ffffff', `Image ${idx}`)
 		};
 	}
 
