f884084cc2fcb823095453d0a7940be589e40e71
Alex Morariu <alexandru.morariu@lgepartner.com>
2022-09-06 10:56:52 +0300

    Fix travis

--- sandstone/custom-skin/src/views/MainPanel.js
+++ sandstone/custom-skin/src/views/MainPanel.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-bind */
-// fix Travis
+
 import Alert from '@enact/sandstone/Alert';
 import BodyText from '@enact/sandstone/BodyText';
 import Button from '@enact/sandstone/Button';
