553ea3e7b829c0518c2983ef4ec9ea89604d9c23
Andrei Tirla <111972354+andreitirlalge@users.noreply.github.com>
2022-11-03 04:39:08 +0200

    WRO-11485: Update package dependencies to fix security vulnerabilities (#184)
    * updated dependencies and migrated to react-router-dom v6
    
    * updated dependencies for individual components
    
    * removed commented code
    
    * replaced double quotes with single
    
    Co-authored-by: Mikyung Kim <mikyung27.kim@lge.com>
    
    * review fixes
    
    * review fixes
    
    * Update agate/all-samples/src/App/App.js
    
    * Apply suggestions from code review
    
    * updated agate/all-samples to latest agate version
    
    * updated all samples to the latest agate version
    
    Co-authored-by: Mikyung Kim <mikyung27.kim@lge.com>
    Co-authored-by: Daniel Stoian <63335068+daniel-stoian-lgp@users.noreply.github.com>

--- agate/all-samples/src/components/SampleItem/SampleItem.js
+++ agate/all-samples/src/components/SampleItem/SampleItem.js
@@ -2,10 +2,10 @@ import Item from '@enact/agate/Item';
 import PropTypes from 'prop-types';
 import {useCallback} from 'react';
 
-const SampleItem = ({children, history, path, ...rest}) => {
+const SampleItem = ({children, navigate, path, ...rest}) => {
 	const itemSelect = useCallback(() => {
-		history.push({pathname: path});
-	}, [history, path]);
+		navigate({pathname: path});
+	}, [navigate, path]);
 
 	return (
 		<Item {...rest} onClick={itemSelect}>
@@ -15,7 +15,7 @@ const SampleItem = ({children, history, path, ...rest}) => {
 };
 
 SampleItem.propTypes = {
-	history: PropTypes.object,
+	navigate: PropTypes.func,
 	path: PropTypes.any
 };
 
