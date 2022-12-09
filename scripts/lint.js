bd9250ab71a4171b0239c70102321cc35cf2e550
Andrei Tirla <andrei.tirla@lgepartner.com>
2022-12-09 11:22:35 +0200

    Updated readdirp dependency and eslint fixes

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,40 +1,31 @@
 /* eslint-disable no-console, no-shadow */
 const process = require('process'),
 	readdirp = require('readdirp'),
-	shell = require('shelljs');
+	shell = require('shelljs'),
+	path = require('path');
 
 let error = false;
 
-function findApps () {
-	return new Promise((resolve, reject) => {
-		readdirp({depth: 2, fileFilter: 'package.json', directoryFilter: ['!node_modules']}, (err, res) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(res.files);
+readdirp
+	.promise('.', {depth: 2, fileFilter: 'package.json', directoryFilter: ['!node_modules']})
+	.then(files => {
+		files.forEach(file => {
+			if (file.path === 'package.json') {
+				return;
+			}
+
+			const directory = path.dirname(file.fullPath);
+			console.log(`Building ${directory}`);
+			shell.cd(directory);
+			const result = shell.exec('npm run lint -- -- --report-unused-disable-directives --max-warnings 0', {silent: true});
+
+			if (result.code !== 0) {
+				console.log(`${file.fullPath} failed lint check: ${result.stdout}`);
+				error = true;
 			}
 		});
-	});
-}
-
-findApps()
-	.then(files =>
-		files.forEach(file => {
-			if (file.parentDir) { // Ignore our own package.json
-				console.log(`Linting ${file.parentDir}`);
-				shell.cd(file.fullParentDir);
-				const result = shell.exec('npm run lint -- -- --report-unused-disable-directives --max-warnings 0', {silent: true});
-				if (result.code !== 0) {
-					console.log(`${file.parentDir} failed lint check: ${result.stdout}`);
-					error = true;
-				}
-			}
-		})
-	);
+	}).catch((err) => console.error(err));
 
 process.on('exit', (code) => {
-	if (!code && !error) {
-		console.log('All clean!');
-	}
 	process.exit(code || error ? 1 : 0);
 });
