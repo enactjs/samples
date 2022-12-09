87ea4ae24f87b53283ccfde53404862eca972fbc
Andrei Tirla <andrei.tirla@lgepartner.com>
2022-12-09 14:39:18 +0200

    revert deleted files index and react-app

---
+++ tutorial-typescript/src/index.tsx
@@ -0,0 +1,18 @@
+import {createRoot, hydrateRoot} from 'react-dom/client';
+import App from './App';
+
+const appElement = (<App />);
+
+// In a browser environment, render instead of exporting
+if (typeof window !== 'undefined') {
+    const container = document.getElementById('root')!;
+
+    // @ts-ignore
+    if (ENACT_PACK_ISOMORPHIC) {
+        hydrateRoot(container, appElement);
+    } else {
+        createRoot(container).render(appElement);
+    }
+}
+
+export default appElement;
