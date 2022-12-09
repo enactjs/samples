7129d1ad57a2205ad8cf3e7981565cf141435ba3
Andrei Tirla <andrei.tirla@lgepartner.com>
2022-12-09 15:03:21 +0200

    code review fixes

--- tutorial-typescript/types/react-app-env.d.ts
+++ tutorial-typescript/types/react-app-env.d.ts
@@ -3,47 +3,47 @@
 /// <reference types="react-dom" />
 
 declare namespace NodeJS {
-    interface ProcessEnv {
-        NODE_ENV: 'development' | 'production' | 'test'
-    }
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test'
+  }
 }
 
 declare module '*.bmp' {
-    const src: string;
-    export default src;
+  const src: string;
+  export default src;
 }
 
 declare module '*.gif' {
-    const src: string;
-    export default src;
+  const src: string;
+  export default src;
 }
 
 declare module '*.jpg' {
-    const src: string;
-    export default src;
+  const src: string;
+  export default src;
 }
 
 declare module '*.jpeg' {
-    const src: string;
-    export default src;
+  const src: string;
+  export default src;
 }
 
 declare module '*.png' {
-    const src: string;
-    export default src;
+  const src: string;
+  export default src;
 }
 
 declare module '*.webp' {
-    const src: string;
-    export default src;
+  const src: string;
+  export default src;
 }
 
 declare module '*.css' {
-    const classes: { [key: string]: string };
-    export default classes;
+  const classes: { [key: string]: string };
+  export default classes;
 }
 
 declare module '*.less' {
-    const classes: { [key: string]: string };
-    export default classes;
+  const classes: { [key: string]: string };
+  export default classes;
 }
