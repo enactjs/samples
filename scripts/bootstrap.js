206c221ba2549a355a83011c601b447bcade76ab
Andrei Tirla <andrei.tirla@lgepartner.com>
2022-12-09 15:29:56 +0200

    updated readdirp to version 3.6.0

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,38 +1,31 @@
 /* eslint-disable no-console, no-shadow */
 const process = require('process'),
 	readdirp = require('readdirp'),
-	shell = require('shelljs');
+	shell = require('shelljs'),
+	path = require('path');
 
 let error = false;
 
-function findApps () {
-	return new Promise((resolve, reject) => {
-		readdirp({depth: 2, fileFilter: 'package.json', directoryFilter: ['!node_modules']}, (err, res) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(res.files);
+readdirp
+	.promise('.', {depth: 2, fileFilter: 'package.json', directoryFilter: ['!node_modules']})
+	.then(files => {
+		files.forEach(file => {
+			if (file.path === 'package.json') {
+				return;
+			}
+
+			const directory = path.dirname(file.fullPath);
+			console.log(`Bootstrapping ${directory}`);
+			shell.cd(directory);
+			const result = shell.exec('npm install && npm run pack', {silent: true});
+
+			if (result.code !== 0) {
+				console.log(`${file.fullPath} failed to build: ${result.stdout}`);
+				error = true;
 			}
 		});
-	});
-}
-
-findApps()
-	.then(files =>
-		files.forEach(file => {
-			if (file.parentDir) { // Ignore our own package.json
-				console.log(`Bootstrapping ${file.parentDir}`);
-				shell.cd(file.fullParentDir);
-				const result = shell.exec('npm install', {silent: true});
-				if (result.code !== 0) {
-					console.log(`${file.parentDir} failed to bootstrap: ${result.stdout}`);
-					error = true;
-				}
-			}
-		})
-	);
+	}).catch((err) => console.error(err));
 
 process.on('exit', (code) => {
 	process.exit(code || error ? 1 : 0);
 });
-
