210be08012d61a54ec03b8222113a6fa027cf778
Stanca Pop <stanca.pop@lgepartner.com>
2023-06-19 18:33:49 +0300

    added different animation for portrait vs landscape

--- sandstone/pattern-view-transitions/src/App/App.js
+++ sandstone/pattern-view-transitions/src/App/App.js
@@ -1,9 +1,10 @@
 import {flushSync} from 'react-dom';
 
+import ImageItem from '@enact/sandstone/ImageItem';
 import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
-import VideoPlayer from '@enact/sandstone/VideoPlayer';
+// import VideoPlayer from '@enact/sandstone/VideoPlayer';
 import {VirtualGridList} from '@enact/sandstone/VirtualList';
-import {Cell} from '@enact/ui/Layout';
+// import {Cell, Layout} from '@enact/ui/Layout';
 import ri from '@enact/ui/resolution';
 import {useCallback, useEffect, useRef, useState} from 'react';
 
@@ -15,14 +16,20 @@ const AppBase = (props) => {
 	const [orientation, setOrientation] = useState(window.screen.orientation.type);
 
 	useEffect(() => {
-		const newOrientation = window.screen.orientation.type;
-
-		document.startViewTransition(() => {
-			flushSync(() => {
-				setOrientation(newOrientation);
+		if (orientation === ('portrait-primary' || 'portrait-secondary')) {
+			document.startViewTransition(() => {
+				flushSync(() => {
+					setOrientation('landscape-primary');
+				});
 			});
-		});
-	}, [orientation]);
+		} else if (orientation === ('landscape-primary' || 'landscape-secondary')) {
+			document.startViewTransition(() => {
+				flushSync(() => {
+					setOrientation('portrait-primary');
+				});
+			});
+		}
+	}, [window.screen.orientation.type]);
 
 	const scrollToRef = useRef(null);
 
@@ -35,12 +42,17 @@ const AppBase = (props) => {
 	}, []);
 
 	const renderItem = useCallback(({index}) => {
+		const getOrientation = window.screen.orientation.type;
 		return (
-			<Cell>
-				<VideoPlayer className={css.player + ' enact-fit'} noAutoPlay poster={videos[index].poster}>
-					<source src={videos[index].source} type="video/mp4" />
-				</VideoPlayer>
-			</Cell>
+			getOrientation === ('landscape-primary' || 'landscape-secondary') ?
+				<ImageItem className={css.player + `-${index}`} src={videos[index].poster}>
+					{videos[index].title}
+				</ImageItem>
+				: getOrientation === ('portrait-primary' || 'portrait-secondary') ?
+				<ImageItem className={css.image + `-${index}`} src={videos[index].poster}>
+					{videos[index].title}
+				</ImageItem>
+				: null
 		);
 	}, []);
 
@@ -57,6 +69,21 @@ const AppBase = (props) => {
 			/>
 		</div>
 	);
+
+	// return (
+	// 	<Layout {...rest} className={className + ' ' + css.app}>
+	// 		{videos.map((video) =>
+	// 			<Cell key={video.id} size="30%">
+	// 				<VideoPlayer className={css.player + ' enact-fit'} noAutoPlay>
+	// 					<source src={video.source} type="video/mp4" />
+	// 					<infoComponents>
+	// 						{video.desc}
+	// 					</infoComponents>
+	// 				</VideoPlayer>
+	// 			</Cell>
+	// 		)}
+	// 	</Layout>
+	// );
 };
 
 const App = ThemeDecorator(AppBase);
