07d1ac2f56e2e17eba3408627512fd05c432b3ee
Stanca Pop <stanca.pop@lgepartner.com>
2023-10-11 14:35:41 +0300

    added missing files for the Sandstone Popup

---
+++ sandstone/pattern-smooth-animations/src/views/SandstonePopup/SandstonePopup.js
@@ -0,0 +1,59 @@
+import {useCallback, useState} from 'react';
+import BodyText from '@enact/sandstone/BodyText';
+import Button from '@enact/sandstone/Button';
+import {Cell, Layout, Row} from '@enact/ui/Layout';
+
+import PopupNoAnimation from '../../components/PopupNoAnimation';
+import PopupSmoothAnimation from '../../components/PopupSmoothAnimation';
+
+import appCss from '../../App/App.module.less';
+import componentCss from './SandstonePopup.module.less';
+
+const SandstonePopup = ({...rest}) => {
+	const [openNoAnimation, setOpenNoAnimation] = useState(false);
+	const [openSmoothAnimation, setOpenSmoothAnimation] = useState(false);
+
+	const toggleNoAnimation = useCallback(() => {
+		setOpenNoAnimation(!openNoAnimation);
+	}, [openNoAnimation]);
+
+	const toggleSmoothAnimation = useCallback(() => {
+		setOpenSmoothAnimation(!openSmoothAnimation);
+	}, [openSmoothAnimation]);
+
+	return (
+		<Layout {...rest} className={componentCss.sandstonePopup} orientation="vertical">
+			<BodyText className={appCss.title}>
+				Starting from Chrome 116, we have the ability to animate &apos;display&apos; and &apos;content-visibility&apos; properties on a keyframe timeline.
+			</BodyText>
+			<Row>
+				<Cell>
+					<BodyText>Click below to open a Popup that has no animation.</BodyText>
+					<Button
+						onClick={toggleNoAnimation}
+					>
+						Open no animation
+					</Button>
+					<PopupNoAnimation
+						openNoAnimation={openNoAnimation}
+						toggleNoAnimation={toggleNoAnimation}
+					/>
+				</Cell>
+				<Cell>
+					<BodyText>Click below to open a Popup that has &apos;noAnimation&apos;, but applies smooth transitions using the new CSS features.</BodyText>
+					<Button
+						onClick={toggleSmoothAnimation}
+					>
+						Open smooth animation
+					</Button>
+					<PopupSmoothAnimation
+						openSmoothAnimation={openSmoothAnimation}
+						toggleSmoothAnimation={toggleSmoothAnimation}
+					/>
+				</Cell>
+			</Row>
+		</Layout>
+	);
+};
+
+export default SandstonePopup;
