c572a45d9ce66677b76d839582c08a542b83591d
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2023-11-15 14:14:06 +0200

    added luna calls to check `theme` key value and created `getSystemSettings` function

--- sandstone/pattern-ls2request-custom-colors/src/views/MainView/MainView.js
+++ sandstone/pattern-ls2request-custom-colors/src/views/MainView/MainView.js
@@ -1,16 +1,18 @@
 import IconItem from '@enact/sandstone/IconItem';
 import {Header, Panel, Panels} from '@enact/sandstone/Panels';
-import {useCallback, useContext, useState} from 'react';
+import LS2Request from '@enact/webos/LS2Request';
+import {useCallback, useContext, useEffect, useState} from 'react';
 
 import ColorCustomizationView from '../ColorCustomizationView';
 import PresetChoiceView from '../PresetChoiceView';
 import ShowcaseView from '../ShowcaseView';
 
-import {AppContext} from '../../constants';
+import {AppContext, customColorsContext} from '../../constants';
+import {getSystemSettings} from '../../lunaCalls/getSystemSettings';
 
 const MainView = (rest) => {
 	// Here we get the context of the app
-	const {context} = useContext(AppContext);
+	const {context, setContext} = useContext(AppContext);
 	const [panelIndex, setPanelIndex] = useState(0);
 
 	// Based on the lightMode variable from our context we determine the appropriate skin for our app
@@ -23,6 +25,39 @@ const MainView = (rest) => {
 	const backward = useCallback(() => {
 		setPanelIndex(panelIndex - 1);
 	}, [panelIndex]);
+
+	// NOTE: this useEffect might be removed in the future, when we'll have a default value for `theme` key
+	useEffect(() => {
+		// On app initialization in webOS environment, call ServiceSettings and check the value for `theme` key
+		if (typeof window === 'object' && window.webOSSystem && window.webOSSystem.launchParams) {
+			new LS2Request().send({
+				service: 'luna://com.webos.service.settings/',
+				method: 'getSystemSettings',
+				parameters: {
+					category: 'customUi',
+					keys: ['theme']
+				},
+				onSuccess: (res) => {
+					// if `theme` key is empty, populate with a default value
+					if (res.settings.theme === '') {
+						new LS2Request().send({
+							service: 'luna://com.webos.service.settings/',
+							method: 'setSystemSettings',
+							parameters: {
+								category: 'customUi',
+								keys: JSON.stringify(customColorsContext)
+							}
+						})
+					}
+				}
+			})
+		}
+	}, []);
+
+	// update app context with fetched data from SettingsService
+	useEffect(() => {
+		getSystemSettings(setContext);
+	}, [setContext]);
 
 	return (
 		<Panels
