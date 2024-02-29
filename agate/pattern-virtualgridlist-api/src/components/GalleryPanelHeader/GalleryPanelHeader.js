1e9a67c8a979db573d6e0c8258abeea626af4ce6
paul-beldean-lgp <77739822+paul-beldean-lgp@users.noreply.github.com>
2024-02-29 08:00:32 +0200

    WRQ-10126: Updated placeholder images URLs (#223)
    * fixed ms virtualgridlist image URL
    
    * updated placeholder image URL
    
    ---------
    
    Co-authored-by: dongsu.won <dongsu.won@lgepartner.com>

--- agate/pattern-virtualgridlist-api/src/components/GalleryPanelHeader/GalleryPanelHeader.js
+++ agate/pattern-virtualgridlist-api/src/components/GalleryPanelHeader/GalleryPanelHeader.js
@@ -21,7 +21,7 @@ const createMockItem = (album, albumSize, showOverlay) => {
 		selected: false,
 		selectionOverlayShowing: showOverlay,
 		caption: album + ' ' + dataLength + caption,
-		source: 'http://via.placeholder.com/300x300/' + color + '/ffffff/png?text=Image ' + dataLength
+		source: 'https://placehold.co/300x300/' + color + '/ffffff/png?text=Image+' + dataLength
 	};
 };
 
