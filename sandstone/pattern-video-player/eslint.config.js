73447ffb228ca91e485ac75a8b9ab33d7997b3b2
taeyoung.hong <taeyoung.hong@lge.com>
2025-04-10 10:00:38 +0900

    fix lint errors

--- sandstone/pattern-video-player/eslint.config.js
+++ sandstone/pattern-video-player/eslint.config.js
@@ -1,13 +1,13 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import { includeIgnoreFile } from "@eslint/compat";
+import {defineConfig, globalIgnores} from "eslint/config";
+import {includeIgnoreFile} from "@eslint/compat";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
+import {fileURLToPath} from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default defineConfig([
-    globalIgnores(["node_modules/*", "build/*", "dist/*"]),
-    includeIgnoreFile(gitignorePath),
+	globalIgnores(["node_modules/*", "build/*", "dist/*"]),
+	includeIgnoreFile(gitignorePath)
 ]);
