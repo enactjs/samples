7676ed9a3d0bd6a77f745931360faac1feb96f20
alexandru-morariu-lgp <alexandru.morariu@lgepartner.com>
2025-06-24 14:56:22 +0300

    Small fix

--- agate/all-samples/src/components/ButtonToSamples/ButtonToSamples.js
+++ agate/all-samples/src/components/ButtonToSamples/ButtonToSamples.js
@@ -5,7 +5,7 @@ import css from './ButtonToSamples.module.less';
 
 const ButtonToSamples = () => (
 	<div className={css.buttonContainer}>
-		<Link to="/" className={css.backLink}>
+		<Link to="../" className={css.backLink}>
 			<Button
 				className={css.backButton}
 				style={{zIndex: 1}}
