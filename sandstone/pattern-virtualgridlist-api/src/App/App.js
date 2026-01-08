c4db72f3b4c1fad4cb6c8e801754c619b4d07586
daniel-stoian-lgp <daniel.stoian@lgepartner.com>
2026-01-08 12:41:26 +0200

    test for nxt-577

--- sandstone/pattern-virtualgridlist-api/src/App/App.js
+++ sandstone/pattern-virtualgridlist-api/src/App/App.js
@@ -1,5 +1,5 @@
 import kind from '@enact/core/kind';
-import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
+import ThemeDecorator from '@enact/limestone/ThemeDecorator';
 
 import MainView from '../views/MainView';
 
